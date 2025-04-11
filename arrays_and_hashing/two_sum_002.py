f09a6ddd95d8fb50ab5f8748ae68bfb4f2a57298
jeddroscz <jeddroscz@gmail.com>
2025-04-11 19:38:04 -0400

    Pushing for testing purposes

---
+++ arrays_and_hashing/two_sum_002.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #We return indexes
+        #Only iterate over the list once for optimal performance O(n) time
+
+        # Attempt to improve runtime by using proper techniques and data structures
+        return
