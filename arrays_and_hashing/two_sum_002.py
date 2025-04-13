eb17f0d3871a824d706a12895b8b676513fd1913
jesussromero <jer423@drexel.edu>
2025-04-13 18:00:03 -0400

    Most effective solution to the two sum problem

--- arrays_and_hashing/two_sum_002.py
+++ arrays_and_hashing/two_sum_002.py
@@ -2,9 +2,21 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #We return indexes
-        #Only iterate over the list once for optimal performance O(n) time
+        # Time complexity: O(n)
+        # Space complexity: O(n)
 
-        # Attempt to improve runtime by using proper techniques and data structures
-        # fixing username
-        return 
+        # First, declare a hashMap/dictionary to store the values and their indices
+        hashMap = {}
+        
+        # Then, iterate through nums and calculate the complement of each number
+        # Then, check if the complement is already in the hashMap
+        # If it is, return the indices of the complement and the current number
+        # Else, add the current number and its index to the hashMap
+        # Finally, return an empty list if no solution is found
+        for i in range(len(nums)):
+            complement = target - nums[i] #4
+            if complement in hashMap:
+                return [hashMap[complement], i]
+            else:
+                hashMap[nums[i]] = i
+        return []
